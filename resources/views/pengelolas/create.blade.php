fa929c39608e9d8408f85d90f0458929a909840e
Dian131303 <dian.99878@gmail.com>
2025-07-08 18:17:52 +0700

    update

--- resources/views/pengelolas/create.blade.php
+++ resources/views/pengelolas/create.blade.php
@@ -261,23 +261,6 @@
                     }
                 });
 
-<<<<<<< HEAD
-    // Phone number formatting
-    // const phoneInput = document.getElementById('no_telp_pengelola');
-    // phoneInput.addEventListener('input', function(e) {
-    //     let value = e.target.value.replace(/\D/g, '');
-    //     if (value.length > 0) {
-    //         if (value.startsWith('0')) {
-    //             // Format: 08xx-xxxx-xxxx
-    //             value = value.replace(/(\d{4})(\d{4})(\d{4})/, '$1-$2-$3');
-    //         } else if (value.startsWith('62')) {
-    //             // Format: +62xxx-xxxx-xxxx
-    //             value = '+' + value.replace(/(\d{2})(\d{3})(\d{4})(\d{4})/, '$1$2-$3-$4');
-    //         }
-    //     }
-    //     e.target.value = value;
-    // });
-=======
                 // Phone number formatting
                 const phoneInput = document.getElementById('no_telp_pengelola');
                 phoneInput.addEventListener('input', function(e) {
@@ -323,7 +306,6 @@
             }
         </script>
     @endpush
->>>>>>> d926ed0a6ff670f876fb98f034c1c996afd3b854
 
     @push('styles')
         <style>
